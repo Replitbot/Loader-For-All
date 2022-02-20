9b7c25a93840455f96cca248eabc1e78f526fd9f
Replitbot <97874310+Replitbot@users.noreply.github.com>
2022-02-21 06:35:13 +0800

    Update Loader For Free.lua

--- Loader For Free.lua
+++ Loader For Free.lua
@@ -4,5 +4,7 @@ elseif game.PlaceId == 7114303730 then -- Dragon Ball Orbz
     a = "https://raw.githubusercontent.com/Replitbot/Loader-For-All/All-Games-Paid-And-Free/Dragon%20Orbz%20balls.lua"
 elseif game.PlaceId == 8448735476 then -- DBZ Adventures
     a = "https://raw.githubusercontent.com/Replitbot/Loader-For-All/All-Games-Paid-And-Free/dbz%20adventures.lua"
+elseif game.Placeid == 5943856429 then
+   a = "https://raw.githubusercontent.com/Replitbot/Loader-For-All/All-Games-Paid-And-Free/Knight%20HEroes"
 end
     loadstring(game:HttpGet(a))()
